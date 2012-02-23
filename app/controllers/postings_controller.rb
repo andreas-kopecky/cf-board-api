93f91d6fd00d50ea5fb0a43f192add20a1e2573e
Andreas Kopecky <andreas.kopecky@gmail.com>
2012-02-23 20:10:00 +0100

    Added create_heading to posting controller, no route yet

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,9 +1,17 @@
 class PostingsController < ApplicationController
-  attr_accessor :posting, :postings, :headings
+  attr_accessor :posting, :postings, :headings, :heading
   respond_to :json
 
   def headings_for_section
     self.headings = Posting.all(conditions: {section_id: params[:section_id], is_heading: true}, order: 'created_at ASC')
     respond_with(self.headings)
+  end
+
+  def create_heading
+    raise 'No valid section_id submitted' unless Section.find params[:section_id]
+    self.heading = Posting.create!(
+      title: params[:title], message: params[:message], user_id: params[:user_id],
+      section_id: params[:section_id], is_heading: true)
+    respond_with(self.heading)
   end
 end
--- spec/controllers/postings_controller_spec.rb
+++ spec/controllers/postings_controller_spec.rb
@@ -9,7 +9,7 @@ describe PostingsController do
   def valid_heading_attributes
     [
       {title: 'Heading 1', message: 'Message 1', user_id: 1, is_heading: true},
-      {title: 'Heading 1', message: 'Message 1', user_id: 1, is_heading: true},
+      {title: 'Heading 2', message: 'Message 2', user_id: 1, is_heading: true},
     ]
   end
 
@@ -25,4 +25,16 @@ describe PostingsController do
       assigns(:headings).should eq(headings)
     end
   end
+
+  describe "POST create_heading" do
+    it "should create a new section within a board" do
+      section = Section.create! valid_section_attributes
+      params = {title: 'Newly Created', message: 'New Messages', user_id: 1, section_id: section.id}
+      post :create_heading, params
+      assigns(:heading).should_not be_nil
+      assigns(:heading).title.should == params[:title]
+      assigns(:heading).message.should == params[:message]
+    end
+  end
+
 end
